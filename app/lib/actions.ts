fda983a227493e3f62ab646fd3c6e13c07f6c163
betadv <62099784+betadv@users.noreply.github.com>
2025-02-25 00:58:50 +0200

    fixed eslint errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -59,8 +59,8 @@ export async function createInvoice(prevState: State, formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
-    // If a database error occurs, return a more specific error.
     return {
+      error,
       message: "Database Error: Failed to Create Invoice.",
     };
   }
@@ -99,7 +99,7 @@ export async function updateInvoice(
       WHERE id = ${id}
     `;
   } catch (error) {
-    return { message: "Database Error: Failed to Update Invoice." };
+    return { error, message: "Database Error: Failed to Update Invoice." };
   }
 
   revalidatePath("/dashboard/invoices");
